dfb7ff78e4b6920ed4f59a36202f1a217a3d94bd
Adam Preston <adam.m.preston@gmail.com>
2013-06-22 00:13:56 -0600

    removed renamed directories

--- cookbooks/tangerine_utility/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'tangerine_utility'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'Apache 2.0'
-description      'Installs/Configures tangerine_utility'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
