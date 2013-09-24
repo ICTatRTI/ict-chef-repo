629f6097bf41d641dc7f2b781c7224fb8c0a886b
Adam Preston <adam.m.preston@gmail.com>
2013-09-24 08:56:03 -0400

    added plain lamp role

--- cookbooks/ict_utility/metadata.rb
+++ cookbooks/ict_utility/metadata.rb
@@ -5,3 +5,11 @@ license          'Apache 2.0'
 description      'Installs/Configures ict_utility'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w{ php apache2 mysql openssl }.each do |cb|
+  depends cb
+end
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
